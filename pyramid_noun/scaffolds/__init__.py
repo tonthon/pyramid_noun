9ae12c1dad327e18b94625d474f764fdc57cc80e
tonthon <tonthon21@gmail.com>
2012-03-01 15:29:28 +0100

    Typo

--- pyramid_noun/scaffolds/__init__.py
+++ pyramid_noun/scaffolds/__init__.py
@@ -6,13 +6,13 @@
 #   License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
 # * Creation Date : 11-01-2012
-# * Last Modified : mer. 11 janv. 2012 22:20:05 CET
+# * Last Modified : ven. 27 janv. 2012 17:20:44 CET
 #
 # * Project :
 #
 
 from pyramid.scaffolds import PyramidTemplate
 
-class MajertiExtensionTemplate(PyramidTemplate):
+class NounExtensionTemplate(PyramidTemplate):
     _template_dir = 'noun'
     summary = "Majerti's pyramid scaffold"
